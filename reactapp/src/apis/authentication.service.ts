12ffe5a733c73e179eb7d47541fbeb260a8267f8
Tri Doan <w.dquoctri@gmail.com>
2022-10-02 22:16:10 +0700

    remove comment

--- reactapp/src/apis/authentication.service.ts
+++ reactapp/src/apis/authentication.service.ts
@@ -20,25 +20,4 @@ export class AuthenticationService extends WebService {
   public logout(): Promise<void> {
     return this.post(AUTH_ENDPOINTS.logout, {})
   }
-
-  //   public static saveToken(token: TokenLike | null): void {
-  //     if (token) {
-  //       window.localStorage.setItem(AuthenticationService.LS_TOKEN_KEY, token.token)
-  //       AuthenticationService.token = token.token
-  //     } else {
-  //       window.localStorage.removeItem(AuthenticationService.LS_TOKEN_KEY)
-  //       AuthenticationService.token = undefined
-  //     }
-  //   }
-
-  //   public static getToken(): Token | null {
-  //     if (!AuthenticationService.token) {
-  //       AuthenticationService.token = window.localStorage.getItem(AuthenticationService.LS_TOKEN_KEY) || undefined
-  //       if (!AuthenticationService.token) {
-  //         return null
-  //       }
-  //     }
-
-  //     return Token.fromJwt(AuthenticationService.token)
-  //   }
 }
